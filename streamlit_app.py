9928011443e3c5ba495d398d64ead1c8918b6a1a
Tanya Everitt <61130138+ttanyaeveritt@users.noreply.github.com>
2024-05-22 08:00:22 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title("Customize your smoothie")
@@ -45,3 +47,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
