45de080b11d8d28ef9ef44d6d851d5fbe120cec2
Tanya Everitt <61130138+ttanyaeveritt@users.noreply.github.com>
2024-05-22 14:11:40 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.write("The name on your smoothie will be: ", name_on_order)
 
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 #Convert the snowpark dataframe to a pandas dataframe so we can use the LOC function 
 pd_df = my_dataframe.to_pandas()
